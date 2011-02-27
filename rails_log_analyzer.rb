a1307f3eaf1d58572c53249ab6a2df078af6e6ee
Damian Baćkowski <damianbackowski@gmail.com>
2011-02-27 22:15:51 +0100

    Work with or without query_stats plugin. Small bug fixes.

--- rails_log_analyzer.rb
+++ rails_log_analyzer.rb
@@ -102,31 +102,31 @@ begin
       controller = $1
       action = $2
       method = $3
-    elsif line =~ /Completed in ([0-9]+)ms \((View: ([0-9]+), DB: ([0-9]+)|DB: ([0-9]+)) ([0-9]+) queries\) \| ([0-9]+)/i
-      total_time = $1.to_i
+    elsif line =~ /Completed in ([0-9]+)ms \((View: ([0-9]+), DB: ([0-9]+)|DB: ([0-9]+))( ([0-9]+) queries){0,1}\) \| ([0-9]+)/i
+      total_time = $1.to_f
 
       unless $3.nil?
         view_time = $3
-        db_time = $4.to_i
-        queries = $6.to_i
-        status = $7.to_i
+        db_time = $4.to_f
+        queries = $7
+        status = $8.to_i
       else
-        db_time = $5.to_i
-        queries = $6.to_i
-        status = $7.to_i
+        db_time = $5.to_f
+        queries = $7
+        status = $8.to_i
       end
-    elsif line =~ /Completed in ([0-9]+)ms \((View: ([0-9]+), DB: ([0-9]+)|DB: ([0-9]+)) ([0-9]+) queries\) \| ([0-9]+)/i
-      total_time = $1.to_i
+    elsif line =~ /Completed in ([0-9]+)ms \((View: ([0-9]+), DB: ([0-9]+)|DB: ([0-9]+))( ([0-9]+) queries){0,1}\) \| ([0-9]+)/i
+      total_time = $1.to_f
       
       unless $3.nil?
         view_time = $3
-        db_time = $4.to_i
-        queries = $6.to_i
-        status = $7.to_i
+        db_time = $4.to_f
+        queries = $7
+        status = $8.to_i
       else
-        db_time = $5.to_i
-        queries = $6.to_i
-        status = $7.to_i
+        db_time = $5.to_f
+        queries = $7
+        status = $8.to_i
       end
     elsif line =~ /500 Error/i
       status = 500
@@ -142,6 +142,8 @@ begin
       end
 
       unless queries.nil?
+        queries = queries.to_i
+        
         unless top_db_queries_actions.has_key?("#{controller}##{action}")
           top_db_queries_actions["#{controller}##{action}"] = {"queries" => queries}
         else
@@ -190,18 +192,22 @@ begin
       end
     end
   }
-  
+
   puts "Top slowest actions:"
   table_output(['Controller', 'Action', 'Total time [seconds]'], top_slowest_actions.map{|a,x| [a.split('#')[0], a.split('#')[1], x["total_time"]/1000]}.sort{|a,b| b[2] <=> a[2]})
 
-  puts "\nTop most DB queries actions:"
-  table_output(['Controller', 'Action', 'Queries count'], top_db_queries_actions.map{|a,x| [a.split('#')[0], a.split('#')[1], x["queries"]]}.sort{|a,b| b[2] <=> a[2]})
+  unless top_db_queries_actions.empty?
+    puts "\nTop most DB queries actions:"
+    table_output(['Controller', 'Action', 'Queries count'], top_db_queries_actions.map{|a,x| [a.split('#')[0], a.split('#')[1], x["queries"]]}.sort{|a,b| b[2] <=> a[2]})
+  end
 
   puts "\nTop most requested actions:"
   table_output(['Controller', 'Action', 'Requested count'], top_most_requested_actions.map{|a,x| [a.split('#')[0], a.split('#')[1], x]}.sort{|a,b| b[2] <=> a[2]}[0 .. options[:limit] - 1])
 
-  puts "\nTop error 500 actions:"
-  table_output(['Controller', 'Action', 'Errors count'], top_error_500_actions.map{|a,x| [a.split('#')[0], a.split('#')[1], x]}.sort{|a,b| b[2] <=> a[2]}[0 .. options[:limit] - 1])
+  unless top_error_500_actions.empty?
+    puts "\nTop error 500 actions:"
+    table_output(['Controller', 'Action', 'Errors count'], top_error_500_actions.map{|a,x| [a.split('#')[0], a.split('#')[1], x]}.sort{|a,b| b[2] <=> a[2]}[0 .. options[:limit] - 1])
+  end
 rescue Exception => e
   puts e
   exit(1)
